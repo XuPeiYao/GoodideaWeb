bfbf86964d0d8eab6082a75510915b3df2291fd0
Pei Yao,Xu <u0124008@nkfust.edu.tw>
2016-04-30 03:04:41 +0800

    系統的圖片背景以及相關連結項目

---
+++ GoodideaJsLib/Banner.ts
@@ -0,0 +1,22 @@
+﻿module goodidea {
+    export class Banner {
+        public id: string;
+        public url: string;
+
+        public static loadFromJSON(data: JSON): Banner {
+            var result = new Link();
+            result.id = data['Id'];
+            result.url = data['Url'];
+            return result;
+        }
+
+        public static async getBannerList(): Promise<Banner[]> {
+            var responseJSON = await postAsync('api/banner/list');
+            var result = [];
+            for (var i = 0; i < responseJSON['Result'].length; i++) {
+                result.push(Banner.loadFromJSON(responseJSON['Result'][i]));
+            }
+            return result;
+        }
+    }
+}
---
+++ GoodideaJsLib/Link.ts
@@ -0,0 +1,24 @@
+﻿module goodidea {
+    export class Link {
+        public id: string;
+        public name: string;
+        public url: string;
+
+        public static loadFromJSON(data: JSON): Link {
+            var result = new Link();
+            result.id = data['Id'];
+            result.name = data['Name'];
+            result.url = data['Url'];
+            return result;
+        }
+
+        public static async getLinkList(): Promise<Link[]> {
+            var responseJSON = await postAsync('api/link/list');
+            var result = [];
+            for (var i = 0; i < responseJSON['Result'].length; i++) {
+                result.push(Link.loadFromJSON(responseJSON['Result'][i]));
+            }
+            return result;
+        }
+    }
+}
