bd1ad9414c6a6aa4129ba0eecdbc3ca94b9dd0ed
Pei Yao,Xu <u0124008@nkfust.edu.tw>
2016-05-05 22:03:29 +0800

    提案瀏覽基本實作

---
+++ GoodideaWeb/scripts/project.ts
@@ -0,0 +1,6 @@
+﻿app.controller('project', async function ($scope, $sce, $uibModal) {
+    $scope.project = await goodidea.Project.getProjectById(queryString['id']);
+    $scope.project.contentSegments = $scope.project.getContentSegments().segments;
+    $scope.$apply();
+
+});
