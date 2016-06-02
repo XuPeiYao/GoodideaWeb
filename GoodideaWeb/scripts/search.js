78892c8c0d79dbf4387031c1aaf07159579d658e
Pei Yao,Xu <u0124008@nkfust.edu.tw>
2016-06-02 22:48:28 +0800

    進階搜尋功能實作

---
+++ GoodideaWeb/scripts/search.js
@@ -0,0 +1,75 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator.throw(value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments)).next());
+    });
+};
+app.controller('search', function ($scope, $sce, $uibModal) {
+    return __awaiter(this, void 0, void 0, function* () {
+        $scope.bannerList = yield goodidea.Banner.getBannerList();
+        if (queryString['q'])
+            $scope.keyword = queryString['q'];
+        $scope.classOptions = [{ id: 'N', name: '不分類' }];
+        $scope.class = 'N';
+        (yield goodidea.Class.getClassList()).forEach(x => $scope.classOptions.push(x));
+        $scope.competitionOptions = [{ id: 'N', name: '不限定' }];
+        $scope.competition = 'N';
+        (yield goodidea.Competition.getCompetitionList(null, null)).forEach(x => $scope.competitionOptions.push(x));
+        $scope.orderOptions = [
+            { id: goodidea.OrderBy.lastEditTime, name: '最後更新時間' },
+            { id: goodidea.OrderBy.name, name: '提案名稱' },
+            { id: goodidea.OrderBy.class, name: '提案分類' },
+            { id: goodidea.OrderBy.views, name: '瀏覽人次' },
+            { id: goodidea.OrderBy.votes, name: '得票數' },
+            { id: goodidea.OrderBy.awardsFirst, name: '獲獎者在前' }
+        ];
+        $scope.order = goodidea.OrderBy.lastEditTime.toString();
+        $scope.$apply(); //通知更新
+        $scope.lastPageResult = null;
+        $scope.projectList = [];
+        $scope.loading = false;
+        $scope.loadNextPage = () => __awaiter(this, void 0, void 0, function* () {
+            $scope.loading = true;
+            if ($scope.lastPageResult == null) {
+                $scope.lastPageResult = yield goodidea.Project.search($scope.keyword, $scope.class == 'N' ? null : $scope.class, $scope.competition == 'N' ? null : $scope.competition, parseInt($scope.order));
+            }
+            else {
+                $scope.lastPageResult = yield $scope.lastPageResult.nextPage();
+            }
+            $scope.lastPageResult.result.forEach(x => {
+                if (!x.cover)
+                    x.cover = $scope.bannerList[0];
+                x.idString = x.id.replace(/\-/g, '');
+                $scope.projectList.push(x);
+            });
+            $scope.loading = false;
+            $scope.$apply(); //通知更新    
+            fixMdlTooltip(document.getElementsByClassName('android-content')[0]);
+        });
+        $scope.reload = () => __awaiter(this, void 0, void 0, function* () {
+            $scope.lastPageResult = null;
+            $scope.projectList = [];
+            yield $scope.loadNextPage();
+            $scope.$apply(); //通知更新  
+        });
+        yield $scope.reload();
+        $scope.$apply(); //通知更新 
+        fixMdlTextfields(document.getElementsByClassName('listController')[0]);
+        //AutoLoadMore
+        var mdlContentElement = document.getElementsByClassName('mdl-layout__content')[0];
+        $scope.autoload = true; //自動讀取鎖定變數
+        mdlContentElement.onscroll = () => __awaiter(this, void 0, void 0, function* () {
+            if (mdlContentElement.scrollHeight - mdlContentElement.scrollTop > 1500)
+                return; //作用座標
+            if (!$scope.autoload)
+                return; //鎖定時不處理
+            $scope.autoload = false; //鎖定
+            yield $scope.loadNextPage(); //讀取
+            $scope.autoload = true; //取消鎖定
+            $scope.$apply(); //通知更新 
+        });
+    });
+});
+//# sourceMappingURL=search.js.map
---
+++ GoodideaWeb/scripts/search.ts
@@ -0,0 +1,71 @@
+﻿app.controller('search', async function ($scope, $sce, $uibModal) {
+    $scope.bannerList = await goodidea.Banner.getBannerList();
+
+    if (queryString['q']) $scope.keyword = queryString['q'];
+
+    $scope.classOptions = [{ id: 'N', name: '不分類' }];
+    $scope.class = 'N';
+    (await goodidea.Class.getClassList()).forEach(x => $scope.classOptions.push(x));
+
+    $scope.competitionOptions = [{ id: 'N', name: '不限定' }];
+    $scope.competition = 'N';
+    (await goodidea.Competition.getCompetitionList(null, null)).forEach(x => $scope.competitionOptions.push(x));
+
+    $scope.orderOptions = [
+        { id: goodidea.OrderBy.lastEditTime, name: '最後更新時間' },
+        { id: goodidea.OrderBy.name, name: '提案名稱' },
+        { id: goodidea.OrderBy.class, name: '提案分類' },
+        { id: goodidea.OrderBy.views, name: '瀏覽人次' },
+        { id: goodidea.OrderBy.votes, name: '得票數' },
+        { id: goodidea.OrderBy.awardsFirst, name: '獲獎者在前' }
+    ];
+    $scope.order = goodidea.OrderBy.lastEditTime.toString();
+    $scope.$apply();//通知更新
+
+    $scope.lastPageResult = null;
+    $scope.projectList = [];
+    $scope.loading = false;
+    $scope.loadNextPage = async () => {
+        $scope.loading = true;
+        if ($scope.lastPageResult == null) {
+            $scope.lastPageResult = await goodidea.Project.search(
+                $scope.keyword,
+                $scope.class == 'N'? null : $scope.class,
+                $scope.competition == 'N' ? null : $scope.competition,
+                parseInt($scope.order)
+            );
+        } else {
+            $scope.lastPageResult = await $scope.lastPageResult.nextPage();
+        }
+        
+        $scope.lastPageResult.result.forEach(x => {
+            if (!x.cover) x.cover = $scope.bannerList[0];
+            x.idString = x.id.replace(/\-/g, '');
+            $scope.projectList.push(x);
+        });
+        $scope.loading = false;
+        $scope.$apply();//通知更新    
+        fixMdlTooltip(document.getElementsByClassName('android-content')[0]);
+    }
+    $scope.reload =async () => {
+        $scope.lastPageResult = null;
+        $scope.projectList = [];
+        await $scope.loadNextPage();
+        $scope.$apply();//通知更新  
+    }
+    await $scope.reload();
+    $scope.$apply();//通知更新 
+    fixMdlTextfields(document.getElementsByClassName('listController')[0]);
+
+    //AutoLoadMore
+    var mdlContentElement: HTMLElement = (<HTMLElement>document.getElementsByClassName('mdl-layout__content')[0]);
+    $scope.autoload = true;//自動讀取鎖定變數
+    mdlContentElement.onscroll = async () => {
+        if (mdlContentElement.scrollHeight - mdlContentElement.scrollTop > 1500) return;//作用座標
+        if (!$scope.autoload) return;//鎖定時不處理
+        $scope.autoload = false;//鎖定
+        await $scope.loadNextPage();//讀取
+        $scope.autoload = true;//取消鎖定
+        $scope.$apply();//通知更新 
+    }
+});
