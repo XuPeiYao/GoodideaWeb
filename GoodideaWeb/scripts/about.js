6bc8e3ca3a52847d613fb69533640582786dac1c
Pei Yao,Xu <u0124008@nkfust.edu.tw>
2016-06-04 18:08:49 +0800

    個人資訊之發起提案Tab提案列表基本顯示

--- GoodideaWeb/scripts/about.js
+++ GoodideaWeb/scripts/about.js
@@ -18,6 +18,30 @@ app.controller('about', function ($scope, $sce, $uibModal) {
             $scope.user = yield goodidea.User.getUserById(queryString['id']);
             $scope.editable = false;
         }
+        $scope.user.htmlContent = $sce.trustAsHtml(markdown.toHtml($scope.user.information));
+        $scope.user.projectList = yield goodidea.Project.getUserProjects($scope.user);
+        var nowTime = new Date();
+        $scope.user.projectList.own.forEach(x => {
+            var time = (nowTime.getTime() + 8 * 3600 * 1000) - x.lastEditTime;
+            var day = Math.floor(time / (24 * 3600 * 1000));
+            time %= 24 * 3600 * 1000;
+            var hours = Math.floor(time / (3600 * 1000));
+            time %= 3600 * 1000;
+            var minnutes = Math.floor(time / (60 * 1000));
+            time %= 60 * 1000;
+            var seconds = Math.floor(time / 1000);
+            var updateString = "";
+            if (day > 0)
+                updateString += `${day}天`;
+            if (hours > 0 && day == 0)
+                updateString += `${hours}時`;
+            if (minnutes > 0 && hours == 0)
+                updateString += `${minnutes}分`;
+            if (seconds > 0 && minnutes == 0)
+                updateString += `${seconds}秒`;
+            updateString += "前更新";
+            x.timeString = updateString;
+        });
         $scope.loading = false;
         $scope.$apply();
         if (queryString['tab']) {
--- GoodideaWeb/scripts/about.ts
+++ GoodideaWeb/scripts/about.ts
@@ -8,6 +8,32 @@
         $scope.user = await goodidea.User.getUserById(queryString['id']);
         $scope.editable = false;
     }
+   
+
+    $scope.user.htmlContent = $sce.trustAsHtml(markdown.toHtml($scope.user.information));
+    $scope.user.projectList = await goodidea.Project.getUserProjects(<goodidea.User>$scope.user);
+    var nowTime = new Date();
+    $scope.user.projectList.own.forEach(x => {
+        var time = (nowTime.getTime() + 8 * 3600 * 1000) - x.lastEditTime;
+        var day = Math.floor(time / (24 * 3600 * 1000));
+        time %= 24 * 3600 * 1000;
+
+        var hours = Math.floor(time / (3600 * 1000));
+        time %= 3600 * 1000;
+
+        var minnutes = Math.floor(time / (60 * 1000));
+        time %= 60 * 1000;
+
+        var seconds = Math.floor(time / 1000);
+        var updateString = "";
+        if (day > 0) updateString += `${day}天`;
+        if (hours > 0 && day == 0) updateString += `${hours}時`;
+        if (minnutes > 0 && hours == 0) updateString += `${minnutes}分`;
+        if (seconds > 0 && minnutes == 0) updateString += `${seconds}秒`;
+        updateString += "前更新";
+
+        x.timeString = updateString;
+    });
     $scope.loading = false;
     $scope.$apply();
 
