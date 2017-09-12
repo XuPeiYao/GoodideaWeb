e31b6bb9796ab3b6be51306243a60e28d594578f
XuPeiYao <xupeiyao@nkfust.edu.tw>
2017-09-12 20:35:09 +0800

    加入不建議變更項目提示

--- GoodideaWeb/scripts/projectEditor.ts
+++ GoodideaWeb/scripts/projectEditor.ts
@@ -163,6 +163,38 @@ function initEditor(selector: string, $scope) {
             ed.on('init', function (ed) {
                 console.log(markdown.toHtml($scope.project.content));
                 tinyMCE.activeEditor.setContent(markdown.toHtml($scope.project.content));
+
+                var ignoreElement = [];
+
+                var headers = tinyMCE.activeEditor
+                    .contentDocument
+                    .querySelectorAll("h1,h2,h3,h4,h5,h6");
+
+                for (var i = 0; i < headers.length; i++) {
+                    headers[i].addEventListener("DOMSubtreeModified", function (e) {
+                        if (ignoreElement.contains(this)) return;
+                        var THIS = this;
+                        swal({
+                            title: "不建議變更的項目",
+                            text: "變更或刪除標題內容，可能導致參賽格式檢查時發生異常，請確保移除的內容並非參賽必要的章節",
+                            type: "warning",
+                            showCancelButton: true,
+                            confirmButtonText: "這個標題不用再提醒",
+                            cancelButtonText: "依舊提醒我，取消變更",
+                            closeOnConfirm: false,
+                            closeOnCancel: false
+                        }, function (isConfirm) {
+                            if (isConfirm) {
+                                console.log(THIS);
+                                ignoreElement.push(THIS);
+                                swal.close();
+                            } else {
+                                tinymce.activeEditor.undoManager.undo();
+                                swal.close();
+                            }
+                        });
+                    }, false);
+                }
             });
         }
     });
