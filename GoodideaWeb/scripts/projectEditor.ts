a2bdd49766a82b7e842cc5cf700718e37f59d5e2
徐培堯 <xupeiyao@nkfust.edu.tw>
2017-09-12 04:53:37 -0500

    移除表格功能
    因表格功能常常跑版

--- GoodideaWeb/scripts/projectEditor.ts
+++ GoodideaWeb/scripts/projectEditor.ts
@@ -16,7 +16,6 @@ function initEditor(selector:string,$scope) {
             edit: { title: 'Edit', items: 'undo redo | cut copy paste pastetext | selectall | searchreplace' },
             insert: { title: 'Insert', items: 'insertUrl insertDocument insertImage insertVideo | charmap hr insertdatetime' },
             format: { title: 'Format', items: 'bold italic superscript subscript | formats | removeformat' },
-            table: { title: 'Table', items: 'inserttable deletetable | row column' },// strikethrough  underline
             view: { title: 'View', items: 'fullscreen' }
         },
         toolbar: "save | cut copy paste | insertfile undo redo | styleselect | bold strikethrough superscript subscript | bullist numlist | hr",
